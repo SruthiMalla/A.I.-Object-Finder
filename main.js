0947fd1f875ef18ea7bc13aa960498d0a1fccc8d
SruthiMalla <52434141+SruthiMalla@users.noreply.github.com>
2022-10-20 16:57:51 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 status = "";
 object_name = "";
+objects = [];
 
 function setup()
 {
@@ -25,4 +26,39 @@ function modelLoaded()
 function draw()
 {
     image(video, 0, 0, 480, 380);
+
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResults);
+
+        for(i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == object_name)
+            {
+                document.getElementById("number_of_objects").innerHTML = "Object Found: " + object_name;
+            }
+            else
+            {
+                document.getElementById("number_of_objects").innerHTML = "Object Not Found: " + object_name;
+            }
+        }
+    }
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+
+    console.log(results);
+    objects = results;
 }
